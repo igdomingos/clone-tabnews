365f3d8c49651f55878a821546781c57a1a0c4d8
Igor Silva <Igordsilvaa@gmail.com>
2026-03-20 23:50:00 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Lara, se amar você fosse um trabalho, eu seria o funcionário do mês todos os meses!\nObrigado por me aguentar e por ser essa pessoa maravilhosa. Te amo um tantão!"! 😘❤️😍</h1>
+    return <h1>Lara, se amar você fosse um trabalho, eu seria o funcionário do mês todos os meses! Obrigado por me aguentar e por ser essa pessoa maravilhosa. Te amo um tantão!"! 😘❤️😍</h1>
 }
 
 export default Home;
